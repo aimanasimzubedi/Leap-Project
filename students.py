23f3e54f1ca6185216a63578789c6a3f1fa78152
aimanasimzubedi <az10271@st.habib.edu.pk>
2024-07-31 21:30:44 +0500

    Update students.py

--- students.py
+++ students.py
@@ -3,9 +3,11 @@ grades={}
 
 def add_student(student_id=None, student_name=None):
     if student_id is None:
-        student_id = input("Enter student ID: ")
-    if student_name is None:
-        student_name = input("Enter student name: ")
+        student_id = int(input("Enter student ID(type -1 to quit): "))
+        while student_id!=-1:
+            if student_name is None:
+                student_name = input("Enter student name: ")
+            break
 
     while student_id not in students:
         students[student_id] = student_name
@@ -13,4 +15,6 @@ def add_student(student_id=None, student_name=None):
         return f"Student {student_name} added with ID {student_id}."
     return f"Student ID {student_id} already exists."
 
+
 print(add_student())
+print(add_student("123","marya"))
